56154aa2bcb01ad97b526dce53e82bc8bd700032
Your Name <you@example.com>
2023-02-12 15:55:23 -0500

    test

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ vex::brain       Brain;
 
 int main() {
 
-    Brain.Screen.printAt( 10, 50, "Hello chainsaw" );
+    Brain.Screen.printAt( 10, 50, "Hello not a chainsaw" );
    
     while(1) {
         
